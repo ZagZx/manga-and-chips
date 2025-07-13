c29f8578510d508b7ce8aa52c03c932542dfd146
ZagZx <azevedodvictor@gmail.com>
2025-07-13 01:50:32 -0300

    arquivo para preparação do ambiente e instalação de dependências

--- build.py
+++ build.py
@@ -1,13 +1,40 @@
 import os
 from secrets import token_hex
+import subprocess
+import sys
 
-ENV_PATH = './.env'
+def beauty_print(string:str):
+    print('='*45)
+    print(string)
+    print('='*45)
 
-if not os.path.exists(ENV_PATH):
+print('Iniciando Build')
+
+dotenv_path = './.env'
+if not os.path.exists(dotenv_path):
     secret_key = token_hex()
 
-    with open(ENV_PATH, 'w') as fw:
+    with open(dotenv_path, 'w') as fw:
         fw.write(f'SECRET_KEY="{secret_key}"\n')
-    print(f'Arquivo .env criado')
+    beauty_print(f'Arquivo .env criado | 1/3')
 else:
-    print(f'.env já existe. Nenhuma alteração feita')
+    beauty_print(f'Arquivo .env já existe | 1/3')
+
+venv_path = './venv'
+if not os.path.exists(venv_path):
+    subprocess.run([sys.executable, '-m', 'venv', venv_path], check=True)
+    beauty_print('Ambiente virtual criado | 2/3')
+else:
+    beauty_print('Ambiente virtual já existe | 2/3')
+
+pip_bin = ''
+if os.name == 'posix':  # Unix (Linux, macOS)
+    pip_bin = os.path.join(venv_path, 'bin', 'pip')
+elif os.name == 'nt':
+    pip_bin = os.path.join(venv_path, 'Scripts', 'pip.exe')
+
+
+requirements_file = 'requirements.txt'
+subprocess.run([pip_bin, 'install', '-r', requirements_file], check=True)
+beauty_print('Dependências instaladas | 3/3')
+
