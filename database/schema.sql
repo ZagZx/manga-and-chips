734573db61a322a92a7f9a2d5151c0936562d358
ZagZx <azevedodvictor@gmail.com>
2025-07-13 00:56:57 -0300

    tabela usuário com comentários para fácil conversão para MySQL

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+# convenção utilizada: tabelas e colunas em inglês e no plural
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT, # AUTO_INCREMENT em MySQL
+    username TEXT NOT NULL, # VARCHAR(255) em MySQL
+    email TEXT NOT NULL UNIQUE, # VARCHAR(255) em MySQL
+    password_hash TEXT NOT NULL # VARCHAR(255) em MySQL
+); 
