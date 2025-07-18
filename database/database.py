50337e6af7915a79c8a60a99dc039ce05ce911a6
ZagZx <azevedodvictor@gmail.com>
2025-07-17 23:38:30 -0300

    run query aceitando params & add_user

--- database/database.py
+++ database/database.py
@@ -1,10 +1,12 @@
 import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
+from query import insert, select
+from typing import Union, Any
 
 class Database:
     def __init__(self, db_path:str) -> None:
         '''
         Parâmetros:
-            - sql_path: Caminho do SQL principal, que contém a criação de todas as tabelas
             - db_path: Caminho e nome do arquivo onde o banco ficará armazenado
         '''
         self.db_path = db_path
@@ -12,22 +14,64 @@ class Database:
 
     @property
     def connection(self) -> sqlite3.Connection:
-        if self._connection or not self.is_open():
-            self._connection = sqlite3.connect(self.db_path)
+        if not self._connection or not self.is_open():
+            try: 
+                self._connection = sqlite3.connect(self.db_path)
+            except Exception as e:
+                print(e)
         return self._connection
     
-    def is_open(self) -> bool:
+    def is_open(self) -> bool: 
         '''Checa se a conexão está aberta'''
         try: # roda um comando e se ele funcionar, a conexão está aberta
-            self._connection.execute('SELECT 1;')
+            self._connection.execute('SELECT 1')
             return True
         except: # se der erro, está fechada
             return False
 
-    def run_sql_file(self, sql_path:str):
+
+    # AJUSTAR RUN QUERY PRA SER UTILIZADO DENTRO DA CLASSE EM FUNÇÕES COMO add_user
+    def run_query(self, sql_query:str, params:Union[tuple[str], str] = None) -> list: # fazer doc depois
+        result = None
+        try:
+            if params:
+                cursor = self.connection.execute(sql_query, params)
+            else:
+                cursor = self.connection.execute(sql_query)
+
+            self.connection.commit()
+            result = cursor.fetchall()
+        except Exception as e: # tirar quando entrar em produção
+            print(e)
+        self.connection.close()
+        return result
+
+    def run_sql_file(self, sql_path:str): # fazer doc
         with open(sql_path) as fr:
             self.connection.executescript(fr.read())
             self.connection.close()
 
+    def add_user(self, username: str, email: str, password: str):
+        '''
+        Adiciona os dados do usuário na tabela users, criptografando a senha
+        
+        Parâmetros:
+            - username: Nome do usuário
+            - email: E-mail do usuário
+            - password: Senha do usuário NÃO CRIPTOGRAFADA
+        '''
+        password_hash = generate_password_hash(password)
+        query = insert('users', ('username', 'email', 'password_hash'))
+
+        self.run_query(query, (username, email, password_hash))
+
 if __name__ == '__main__':
-    db = Database('./database/schema.sql', './database/database.db')
+    db = Database('./database/database.db')
+
+    db.run_query("DELETE FROM sqlite_sequence WHERE name='users'") # tira os ids no autoincrement, só pra deixar bonito no sqlite viewer
+    db.run_query("DELETE FROM users")
+    db.add_user('ZagZ', 'azevedodvictor@gmail.com', 'senhaBEMdificil')
+
+    print(db.run_query('SELECT * FROM users'))
+
+    # print(db.run_query(select('users', 'id')))
---
+++ database/query.py
@@ -0,0 +1,50 @@
+from typing import Union
+
+def insert(table: str, rows: Union[tuple[str], list[str], str]) -> str:
+    '''
+    Formata uma string para um insert SQL com placeholders(?) equivalentes a quantidade de linhas
+
+    Parâmetros:
+        - table: Nome da tabela
+        - rows: Tupla com o nome das linhas ("username", "email", "password_hash") ou caso seja uma única linha, apenas o nome da linha em string "username"
+
+    Retorna:
+        - Uma string de INSERT como essa: "INSERT INTO users(username, email, password_hash) VALUES (?, ?, ?)"
+    '''
+    placeholders = '?'
+    if isinstance(rows, (list, tuple)):
+        placeholders = ', '.join(['?'] * len(rows))
+        rows = ', '.join(rows)
+        
+    return f'INSERT INTO {table}({rows}) VALUES ({placeholders})'
+
+
+def select(table: str, rows: Union[tuple[str], list[str], str] = "*") -> str:
+    '''
+    Formata uma string para um SELECT SQL.
+
+    Parâmetros:
+        - table: Nome da tabela
+        - rows: Tupla com os nomes das colunas ("id", "username"), caso não seja informado o parâmetro, o mesmo será "*", que selecionará todas as linhas de uma tabela
+
+    Retorna:
+        - Uma string de SELECT como esta: "SELECT id, username FROM users"
+          ou, se o valor padrão for usado: "SELECT * FROM users"
+    '''
+    if isinstance(rows, (list, tuple)):
+        rows = ', '.join(rows)
+        
+    return f'SELECT {rows} FROM {table}'
+
+
+
+
+if __name__ == '__main__':
+    print()
+    print(insert('users', 'username'))
+    print()
+    print(insert('users', ['username', 'email', 'password_hash']))
+    print()
+    print(select('users'))
+    print()
+    print(select('users', ('id', 'username')))
