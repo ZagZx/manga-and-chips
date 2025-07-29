f782249eff50090e4b4c6c9de5e07d89e618a0c9
Pedro Victor <azevedodvictor@gmail.com>
2025-07-29 12:21:25 -0300

    tratamento de erro na busca de mangás

--- utils/api/search.py
+++ utils/api/search.py
@@ -2,9 +2,12 @@ import requests
 
 BASE_URL = 'https://api.mangadex.org'
 
-def search_manga_by_title(title:str) -> list[int]:
-    return requests.get(f'{BASE_URL}/manga?title={title}').json()['data']
-
+def search_manga_by_title(title:str) -> list[dict]:
+    try:
+        return requests.get(f'{BASE_URL}/manga?title={title}').json()['data']
+    except Exception as e:
+        print(f'Erro ao pesquisar mangás: {e}')
+        return []
 # if __name__ == '__main__':
     # import json
 
