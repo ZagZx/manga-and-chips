2ecaa28a4afe49568fce67818a83fcec18c39e32
ZagZx <azevedodvictor@gmail.com>
2025-08-08 00:32:37 -0300

    fix importação circular

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
 from .user import User
 from .user_library import UserLibrary
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
