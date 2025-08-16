a5762e780e99685eed44658e96b4335c51d82909
ZagZx <azevedodvictor@gmail.com>
2025-08-16 19:06:17 -0300

    atualização dos filtros funcionando

--- app/routes/user/settings.py
+++ app/routes/user/settings.py
@@ -1,11 +1,41 @@
-from flask import redirect, url_for, current_app, request, render_template
+from flask import redirect, url_for, request, render_template
 from flask_login import login_required, current_user
 
-from app.models import UserLibrary
+from app.models import UserSettings
 from app import db
 
 from . import user_bp
 
 @user_bp.route('/settings')
+@login_required
 def settings():
-    return render_template('settings.html')
+    user_settings:UserSettings = UserSettings.query.filter_by(user_id=current_user.id).first()
+    content_ratings = {
+        'safe':user_settings.safe,
+        'suggestive':user_settings.suggestive,
+        'erotica':user_settings.erotica,
+        'pornographic':user_settings.pornographic
+    }
+
+    return render_template('settings.html', filters=content_ratings)
+
+@user_bp.route('/settings/update', methods = ['POST'])
+@login_required
+def update_settings():
+    safe = bool(request.form.get('safe'))
+    suggestive = bool(request.form.get('suggestive'))
+    erotica = bool(request.form.get('erotica'))
+    pornographic = bool(request.form.get('pornographic'))
+
+    user_settings:UserSettings = UserSettings.query.filter_by(user_id=current_user.id).first()
+    if user_settings.safe != safe:
+        user_settings.safe = safe
+    if user_settings.suggestive != suggestive:
+        user_settings.suggestive = suggestive
+    if user_settings.erotica != erotica:
+        user_settings.erotica = erotica
+    if user_settings.pornographic != pornographic:
+        user_settings.pornographic = pornographic
+    db.session.commit()
+
+    return redirect(url_for('index.index'))
