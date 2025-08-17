c05d4d88771942a119b96bcdc0e42e3214770348
ZagZx <azevedodvictor@gmail.com>
2025-08-16 22:28:02 -0300

    alteração de senha funcionando

--- app/routes/user/settings.py
+++ app/routes/user/settings.py
@@ -1,5 +1,6 @@
-from flask import redirect, url_for, request, render_template, flash
+from flask import redirect, url_for, request, render_template, flash, get_flashed_messages
 from flask_login import login_required, current_user, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.models import User, UserSettings
 from app import db
@@ -39,6 +40,31 @@ def update_settings():
     db.session.commit()
 
     return redirect(url_for('index.index'))
+
+@user_bp.route('/settings/password', methods = ['GET', 'POST'])
+@login_required
+def change_password():
+    if request.method == 'POST':
+        password = request.form.get('password')
+        new_passwords = [request.form.get('new-password-1'), request.form.get('new-password-2')]
+
+        if not check_password_hash(current_user.password_hash, password):
+            flash('A senha atual está incorreta', 'error')
+        if new_passwords[0] != new_passwords[1]:
+            flash('As novas senhas não correspondem uma com a outra','error')
+        if password == new_passwords[0] or password == new_passwords[1]:
+            flash('Sua nova senha não pode ser igual a atual','error')
+
+        if not get_flashed_messages(category_filter='error'):
+            user:User = User.query.get(current_user.id)
+            print(user)
+            user.password_hash = generate_password_hash(new_passwords[0])
+            
+            db.session.commit()
+
+            return redirect(url_for('user.settings'))
+        
+    return render_template('settings/password.html')
 
 @user_bp.route('/settings/delete-account', methods = ['GET', 'POST'])
 @login_required
