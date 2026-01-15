76783e9b5086f562342dcb01eb69ca6061cdf153
ZagZx <azevedodvictor@gmail.com>
2026-01-15 16:28:57 -0300

    feat: componente de botão com estilos diferentes se primário ou secundário

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,16 +1,26 @@
-import { ReactNode } from "react";
+import clsx, { ClassValue } from "clsx";
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
+
+type ButtonStyle = "primary" | "secondary"
 
 interface ButtonProps {
   children: ReactNode;
+  style?: ButtonStyle;
+  extraClassNames?: ClassValue;
 }
 
-export default function Button({ children }: ButtonProps) {
+export default function Button({ children, style="primary", extraClassNames  }: ButtonProps) {
   return(
-    <button className="
-      bg-primary-400 
-      text-white font-bold 
-      hover:bg-primary-600 hover:cursor-pointer
-      rounded-2xl py-2">
+    <button className={clsx(
+      "hover:cursor-pointer",
+      "rounded py-2",
+      "font-bold",
+      "border-2",
+      style === "primary" ? ("bg-primary hover:bg-primary-hover text-secondary border-transparent") : null,
+      style === "secondary" ? ("bg-secondary hover:bg-secondary-hover text-primary border-primary  hover:border-primary-hover") : null,
+      extraClassNames,
+      )}
+    >
       {children}
     </button>
   );
