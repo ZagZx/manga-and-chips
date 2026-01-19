346bb2e622c838092a17244d33ad186efa1d5ebc
ZagZx <azevedodvictor@gmail.com>
2026-01-19 08:23:00 -0300

    style: configuração da variavel color-background no globals

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="w-50 h-50 flex flex-col bg-black gap-3">
+      <div className="w-50 h-50 flex flex-col bg-background gap-3">
         <Button>Primário</Button>
         <Button style="secondary">Secundário</Button>
       </div>
