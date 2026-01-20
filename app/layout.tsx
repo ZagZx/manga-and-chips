40bb6c6c10c9a225596afe59fc5b8bb4cabfd732
ZagZx <azevedodvictor@gmail.com>
2026-01-20 08:32:18 -0300

    feat: Footer adicionado no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/Header";
+import Footer from "@/components/ui/Footer";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         <main className="flex-1 py-4">
           {children}
         </main>
-
+        <Footer />
       </body>
     </html>
   );
